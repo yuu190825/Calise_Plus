fb3c1071f6b621fd062b7181e76a84045825515c
Alice <yuu190825@icloud.com>
2023-03-08 10:43:34 +0800

    Initial Commit

---
+++ CalisePlus/CalisePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalisePlusApp.swift
+//  CalisePlus
+//
+//  Created by 王ユウ on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct CalisePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalisePlusUITests/CalisePlusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalisePlusUITestsLaunchTests.swift
+//  CalisePlusUITests
+//
+//  Created by 王ユウ on 2023/03/08.
+//
+
+import XCTest
+
+final class CalisePlusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
