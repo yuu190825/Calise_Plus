690efde9edd909879844b6b59b08aa0b2100e53f
Alice <yuu190825@icloud.com>
2023-03-11 21:01:21 +0800

    M20311_2101

--- CalisePlus/ContentView.swift
+++ CalisePlus/ContentView.swift
@@ -13,13 +13,10 @@ struct ContentView: View {
         ZStack {
             Color.gray.ignoresSafeArea()
             VStack(spacing: 6) {
-                HStack(spacing: 6) {
-                    Text("\(screenText)")
-                        .font(.system(size: 36, weight: .bold))
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
-                        .foregroundColor(.black)
-                        .background(.white)
-                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                Text("\(screenText)")
+                    .font(.system(size: 36, weight: .bold))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                    .background(.white).foregroundColor(.black)
                 HStack(spacing: 6) {
                     VStack {
                         Slider(value: $oouControl, in: 0...2, step: 1, onEditingChanged: {
@@ -33,23 +30,21 @@ struct ContentView: View {
                                 oouControlText = "4/5"
                             }
                             calc.setOouControl(Int32(oouControl))
-                        })
+                        }).frame(maxWidth: .infinity, maxHeight: .infinity)
                         Text("\(oouControlText)")
                             .font(.system(size: 24, weight: .bold))
-                            .frame(width: 50, height: 40)
-                            .foregroundColor(.white)
-                            .background(.black)
+                            .frame(maxWidth: 50, maxHeight: .infinity)
+                            .background(.black).foregroundColor(.white)
                     }.frame(maxWidth: .infinity, maxHeight: .infinity)
                     VStack {
                         Slider(value: $dotControl, in: 0...3, step: 1, onEditingChanged: {
                             editing in
                             dotControlText = calc.setDotControl(Int32(dotControl))
-                        })
+                        }).frame(maxWidth: .infinity, maxHeight: .infinity)
                         Text("\(dotControlText)")
                             .font(.system(size: 24, weight: .bold))
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.white)
-                            .background(.black)
+                            .frame(maxWidth: 50, maxHeight: .infinity)
+                            .background(.black).foregroundColor(.white)
                     }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
@@ -57,150 +52,127 @@ struct ContentView: View {
                         Text("C").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.myNegative()}) {
                         Text("(-)").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.backSpace()}) {
                         Text("<-").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
                     Button(action: {screenText = calc.memoryRead()}) {
                         Text("MR").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {calc.memoryWrite()}) {
                         Text("MW").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.powTwo()}) {
                         Text("^2").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.mySqrt()}) {
                         Text("SR").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
                     Button(action: {screenText = calc.sendNumber("7")}) {
                         Text("7").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("8")}) {
                         Text("8").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("9")}) {
                         Text("9").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendOperator(4)}) {
                         Text("/").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
                     Button(action: {screenText = calc.sendNumber("4")}) {
                         Text("4").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("5")}) {
                         Text("5").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("6")}) {
                         Text("6").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendOperator(3)}) {
                         Text("*").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
                     Button(action: {screenText = calc.sendNumber("1")}) {
                         Text("1").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("2")}) {
                         Text("2").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendNumber("3")}) {
                         Text("3").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendOperator(2)}) {
                         Text("-").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 HStack(spacing: 6) {
                     Button(action: {screenText = calc.sendNumber("0")}) {
                         Text("0").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendDot()}) {
                         Text(".").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.getResult()}) {
                         Text("=").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                     Button(action: {screenText = calc.sendOperator(1)}) {
                         Text("+").font(.system(size: 36, weight: .bold))
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .foregroundColor(.white)
-                    .background(.black)
+                    .background(.black).foregroundColor(.white)
                 }.frame(maxWidth: .infinity, maxHeight: .infinity)
             }.frame(maxWidth: .infinity, maxHeight: .infinity).padding(6)
         }
