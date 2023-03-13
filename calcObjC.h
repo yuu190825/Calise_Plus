bc1263630ae81ace9970ddd76a011f23e5fe203f
Alice <yuu190825@icloud.com>
2023-03-13 20:25:40 +0800

    M20313_2025

--- calcObjC.h
+++ calcObjC.h
@@ -5,19 +5,19 @@
 
 @interface calcObjC : NSObject
 
-- (void) setOouControl : (int) myValue;
-- (NSString *) setDotControl : (int) myValue;
-- (NSString *) myClear;
-- (NSString *) myNegative;
-- (NSString *) backSpace;
-- (NSString *) memoryRead;
-- (void) memoryWrite;
-- (NSString *) powTwo;
-- (NSString *) mySqrt;
-- (NSString *) sendNumber : (NSString *) mySend;
-- (NSString *) sendOperator : (int) mySend;
-- (NSString *) sendDot;
-- (NSString *) getResult;
+- (void)setOouControl: (int)myValue;
+- (NSString*)setDotControl: (int)myValue;
+- (NSString*)myClear;
+- (NSString*)myNegative;
+- (NSString*)backSpace;
+- (NSString*)memoryRead;
+- (void)memoryWrite;
+- (NSString*)powTwo;
+- (NSString*)mySqrt;
+- (NSString*)sendNumber: (NSString*)mySend;
+- (NSString*)sendOperator: (int)mySend;
+- (NSString*)sendDot;
+- (NSString*)getResult;
 
 @end
 
--- calcObjC.mm
+++ calcObjC.mm
@@ -5,76 +5,76 @@
     Calc *calc;
 }
 
-- (id) init {
+- (id)init {
     self = [super init];
     calc = new Calc();
     return self;
 }
 
-- (void) dealloc {
+- (void)dealloc {
     delete calc;
 }
 
-- (void) setOouControl : (int) myValue {
-    calc -> setOouControl(myValue);
+- (void)setOouControl: (int)myValue {
+    calc->setOouControl(myValue);
 }
 
-- (NSString *) setDotControl : (int) myValue {
-    string myReturn = calc -> setDotControl(myValue);
+- (NSString*)setDotControl: (int)myValue {
+    string myReturn = calc->setDotControl(myValue);
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) myClear {
-    string myReturn = calc -> myClear();
+- (NSString*)myClear {
+    string myReturn = calc->myClear();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) myNegative {
-    string myReturn = calc -> myNegative();
+- (NSString*)myNegative {
+    string myReturn = calc->myNegative();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) backSpace {
-    string myReturn = calc -> backSpace();
+- (NSString*)backSpace {
+    string myReturn = calc->backSpace();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) memoryRead {
-    string myReturn = calc -> memoryRead();
+- (NSString*)memoryRead {
+    string myReturn = calc->memoryRead();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (void) memoryWrite {
-    calc -> memoryWrite();
+- (void)memoryWrite {
+    calc->memoryWrite();
 }
 
-- (NSString *) powTwo {
-    string myReturn = calc -> powTwo();
+- (NSString*)powTwo {
+    string myReturn = calc->powTwo();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) mySqrt {
-    string myReturn = calc -> mySqrt();
+- (NSString*)mySqrt {
+    string myReturn = calc->mySqrt();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) sendNumber : (NSString *) mySend {
-    string myReturn = calc -> sendNumber([mySend UTF8String]);
+- (NSString*)sendNumber: (NSString*)mySend {
+    string myReturn = calc->sendNumber([mySend UTF8String]);
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) sendOperator : (int) mySend {
-    string myReturn = calc -> sendOperator(mySend);
+- (NSString*)sendOperator: (int)mySend {
+    string myReturn = calc->sendOperator(mySend);
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) sendDot {
-    string myReturn = calc -> sendDot();
+- (NSString*)sendDot {
+    string myReturn = calc->sendDot();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
-- (NSString *) getResult {
-    string myReturn = calc -> getResult();
+- (NSString*)getResult {
+    string myReturn = calc->getResult();
     return [NSString stringWithUTF8String:myReturn.c_str()];
 }
 
